d556a440b5523a28c92b2c0cacd76697bddef0ba
gabrielPereira-ipcb <gsantana99.gs@gmail.com>
2024-10-21 12:41:00 +0100

    segundo commit script3xml

--- script3Xml.php
+++ script3Xml.php
@@ -3,25 +3,10 @@
 include_once("base_dados.php");
 $conn = conectar_bd();
 
-$sql = 'SHOW DATABASES';
-$result = $conn->query($sql);
+$fp = fopen("camionagem.xml","w");
 
-echo '<?xml version="1.0" encoding="ISO-8859-1"?>';
-echo '<empresa_camionagem>';
+fprintf($fp,"<?xml version='1.0' encoding='UTF-8'?>\n");
+fprintf($fp,"<camionistas>\n");
 
-/*
-if($result -> num_rows > 0){
-    while($row = $result -> fetch_assoc()){
-        echo '<motorista>';
-        echo '<nome>' ($row['nome'])
-    }
-}
-*/
-
-//$set = mysql_query('SHOW DATABASES;');
-while ($row = $result -> fetch_array())
-{
-   echo $row;
-}
-
+$resultadoTabelas = $conn-> query("SHOW TABLES");
 ?>
