f77162fe69b9a959fdcde4f25c729867f2092802
gabrielPereira-ipcb <gsantana99.gs@gmail.com>
2024-10-21 13:41:08 +0100

    terceiro commit

--- script3Xml.php
+++ script3Xml.php
@@ -6,7 +6,36 @@ $conn = conectar_bd();
 $fp = fopen("camionagem.xml","w");
 
 fprintf($fp,"<?xml version='1.0' encoding='UTF-8'?>\n");
-fprintf($fp,"<camionistas>\n");
+fprintf($fp,"<camionagem>\n");
 
-$resultadoTabelas = $conn-> query("SHOW TABLES");
+$mostraTabela = $conn-> query("SHOW TABLES");
+if ($mostraTabela->num_rows > 0) {
+    while ($row = $mostraTabela->fetch_array()) {
+        $nomeTabela = $row[0];
+
+        $sql = "SELECT * FROM $nomeTabela";
+        $tabelas = $conn->query($sql);
+
+        if ($tabelas->num_rows > 0) {
+            $colunas = array_keys($tabelas->fetch_assoc());
+            $tabelas->data_seek(0);
+
+            while ($row = $tabelas->fetch_assoc()) {
+                fprintf($fp,"\t<$nomeTabela>\n");
+
+                foreach ($colunas as $coluna) {
+                    fprintf($fp,"\t\t<$coluna>$row[$coluna]</$coluna>\n");
+                }
+
+                fprintf($fp,"\t</$nomeTabela>\n");
+
+            }
+        }
+    }
+}
+fprintf($fp,"</camionagem>\n");
+fclose($fp);    
+
+echo "criado com sucesso.";
+$conn ->close();
 ?>
